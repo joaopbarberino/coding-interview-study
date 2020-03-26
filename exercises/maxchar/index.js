4b6417322e6fb29d1bdb66d7c77c425f81455210
João Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-03-26 17:03:53 -0300

    Add "maxchar" problem solution

---
+++ exercises/maxchar/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    let charMap = {}, mostUses = 0, mostUsed;
+
+    for (let char of str.split('')) {
+        charMap[char] ? charMap[char] += 1 : charMap[char] = 1;
+    }
+
+    for(let char in charMap) {
+      if(charMap[char] > mostUses) {
+        mostUses = charMap[char];
+        mostUsed = char;
+      }
+    }
+    
+    return mostUsed;
+}
+
+module.exports = maxChar;
